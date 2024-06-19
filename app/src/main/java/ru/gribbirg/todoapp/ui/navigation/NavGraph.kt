0446689b2f1a2eb3f8e3e4287a5a32eccdcd8d35
Alexander Gribkov <gribkovalexander@gmail.com>
2024-06-19 20:45:49 +0300

    feat: navigation animations

--- app/src/main/java/ru/gribbirg/todoapp/ui/navigation/NavGraph.kt
+++ app/src/main/java/ru/gribbirg/todoapp/ui/navigation/NavGraph.kt
@@ -1,5 +1,10 @@
 package ru.gribbirg.todoapp.ui.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -17,9 +22,23 @@ fun NavGraph(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.List.route
+        startDestination = Screen.List.route,
     ) {
-        composable(Screen.List.route) {
+        composable(
+            Screen.List.route,
+            enterTransition = {
+                fadeIn(
+                    animationSpec = tween(durationMillis = 300),
+                    initialAlpha = 0.999f
+                )
+            },
+            exitTransition = {
+                fadeOut(
+                    animationSpec = tween(durationMillis = 300),
+                    targetAlpha = 0.999f
+                )
+            },
+        ) {
             TodoListItemScreen(
                 viewModel = listViewModel,
                 toEditItemScreen = { item ->
@@ -28,8 +47,30 @@ fun NavGraph(
                 }
             )
         }
-        composable(Screen.Edit.route) {
-            EditItemScreen(viewModel = editItemViewModel)
+        composable(
+            Screen.Edit.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                    animationSpec = tween(
+                        300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.End,
+                    animationSpec = tween(
+                        300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+        ) {
+            EditItemScreen(
+                viewModel = editItemViewModel,
+                onClose = { navController.popBackStack() })
         }
     }
 }
