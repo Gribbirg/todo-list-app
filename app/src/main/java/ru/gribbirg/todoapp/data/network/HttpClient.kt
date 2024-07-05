447757a22e2a45e57e7d13b28bce19b03d58624b
Alexander Gribkov <gribkovalexander@gmail.com>
2024-07-06 00:04:35 +0300

    fix: log remove

--- app/src/main/java/ru/gribbirg/todoapp/data/network/HttpClient.kt
+++ app/src/main/java/ru/gribbirg/todoapp/data/network/HttpClient.kt
@@ -1,15 +1,10 @@
 package ru.gribbirg.todoapp.data.network
 
-import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.isSuccess
@@ -44,20 +39,21 @@ val httpClient = HttpClient(Android) {
         }
     }
 
-    install(Logging) {
-        level = LogLevel.ALL
-        logger = object : Logger {
-            override fun log(message: String) {
-                Log.v("internet", "Ktor: $message")
-            }
-        }
-    }
-
-    install(ResponseObserver) {
-        onResponse { response ->
-            Log.d("internet", "HTTP status: ${response.status.value}")
-        }
-    }
+    // TODO: Remove before release
+    //install(Logging) {
+    //    level = LogLevel.ALL
+    //    logger = object : Logger {
+    //        override fun log(message: String) {
+    //            Log.v("internet", "Ktor: $message")
+    //        }
+    //    }
+    //}
+    //
+    //install(ResponseObserver) {
+    //    onResponse { response ->
+    //        Log.d("internet", "HTTP status: ${response.status.value}")
+    //    }
+    //}
 
     install(DefaultRequest) {
         contentType(ContentType.Application.Json)
